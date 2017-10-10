b81f3271a5406527883697f636d80e06df9a1f23
Cal <atl.calgordon@gmail.com>
2017-10-10 02:12:17 -0400

    functioning version

--- runnable/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = '0000';
-$db = 'smartvid';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
