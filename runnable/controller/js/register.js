fb4e816a25736e5c8317233aac2eb8769b94ad45
Cal <atl.calgordon@gmail.com>
2017-10-10 02:46:07 -0400

    added minor function in register.js

--- runnable/controller/js/register.js
+++ runnable/controller/js/register.js
@@ -1,5 +1,19 @@
 function onClick(){
+	
+	if (document.getElementById("acceptTerms").checked == true){
 
-	alert("hi test");
+		var first =	document.getElementById("firstName").value;
+		var last = document.getElementById("lastName").value;
+		var email = document.getElementById("email").value;
+		var pass = document.getElementById("pass").value;
+		var passConf = document.getElementById("passConf").value;
+	
+		alert("First: " + first + " | Last: " + last + " | Email: " + email + " | Password: " + pass + " | Confirm Password: " + passConf);
+	
+	}else{
+		
+		alert("You must accept the terms and conditions to continue");
+		
+	}
 
 }
