7f3b17cec9f6568f41787eca04e75475226e7031
Alex McWhorter <amcwhor1@ggc.edu>
2017-10-21 16:55:45 -0400

    Created about page and meet the team page

---
+++ model/about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartVid</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
+    <link rel="stylesheet" href="assets/css/Dynamically-Queue-Videos.css">
+    <link rel="stylesheet" href="assets/css/Features-Blue.css">
+    <link rel="stylesheet" href="assets/css/Features-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean1.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button1.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Team-Grid.css">
+    <link rel="stylesheet" href="assets/css/Team-with-rotating-cards.css">
+</head>
+
+<body id="body">
+    <nav class="navbar navbar-default navigation-login" id="LoginNav">
+        <div class="container">
+            <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">SmartVid </a>
+                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            </div>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="active" role="presentation"><a href="Main.html">Main </a></li>
+                    <li role="presentation"><a href="#">About </a></li>
+                </ul>
+                <p class="navbar-text navbar-right actions"><a class="navbar-link login" href="Main.html">Logout </a> <a class="btn btn-primary action-button" role="button" href="ProfDash.html">Home </a></p>
+            </div>
+        </div>
+    </nav>
+    <div id="head">
+        <h1 class="text-primary" style="padding:0px;padding-bottom:-5px;margin:23px;height:0px;">What is SmartVid?</h1>
+        <p class="text-center" id="smartvidOverview" style="font-size:19px;/*background-color:rgba(25,25,31,0.54);*/width:1094px;max-width:-1px;min-width:1px;padding:4px;padding-right:1px;padding-top:0px;padding-left:119px;color:rgb(238,238,241);margin:83px;height:31px;">SmartVid is an educational platform for instructors to upload video content to help their students learn material at their own pace. Click the button below to meet the Brogrammers or check out some of our awesome features. Thank you for choosing
+            SmartVid to help making learning great again!</p><a class="btn btn-default btn-lg" role="button" href="#" id="meetTheTeam" style="margin:-2px;padding:5px;padding-bottom:15px;padding-top:9px;height:41px;font-size:16px;">Meet the Team</a></div>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="margin:8px;">
+                    <h2 class="text-primary" id="features" style="padding-bottom:20px;padding-left:2px;padding-top:27px;background-color:rgba(255,255,255,0);width:1138px;margin:10px;"><strong>Top Professor Features</strong> </h2></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4"><i class="glyphicon glyphicon-upload" style="font-size:45px;padding-bottom:-12px;"></i>
+                    <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Upload Videos</h3>
+                    <p id="uploadDescription">Professors can upload as many video lessons as they wish for their students to learn.</p>
+                </div>
+                <div class="col-lg-4 col-md-4" id="account"><i class="glyphicon glyphicon-lock" style="font-size:45px;padding-bottom:-12px;"></i>
+                    <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Protected Account</h3>
+                    <p>Students and professors both have password protected accounts, But as a professor, no one else will be able to manage your class sections</p>
+                </div>
+                <div class="col-lg-4 col-md-4" id="gradeBook"><i class="glyphicon glyphicon-check" style="font-size:45px;padding-bottom:-12px;"></i>
+                    <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Gradebook </h3>
+                    <p>Professors will be able to see student's viewing habits with a percentage (%) of how much of the lessons your student has watched.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="margin:8px;">
+                    <h2 class="text-primary" id="features" style="padding-top:26px;padding-bottom:21px;"><strong>Top Student Features</strong> </h2></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4"><i class="glyphicon glyphicon-facetime-video" style="font-size:45px;padding-bottom:-12px;"></i>
+                    <h3 style="padding:-15px;padding-top:-18px;margin:3px;">View Videos</h3>
+                    <p>Students can watch uploaded professor content at their own pace to ensure that they are understanding the material </p>
+                </div>
+                <div class="col-lg-4 col-md-4"><i class="glyphicon glyphicon-repeat" style="font-size:45px;padding-bottom:-12px;"></i>
+                    <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Rewatch Videos</h3>
+                    <p>The beauty of video lessons, is you can watch them as many time as you like until you get it!</p>
+                </div>
+                <div class="col-lg-4 col-md-4"><i class="glyphicon glyphicon-list-alt" style="font-size:45px;padding-bottom:-12px;"></i>
+                    <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Quizzes </h3>
+                    <p>Students can take comprensive quizzes following their video to check for understanding before moving on to the next one.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer id="foot">
+        <ul class="list-inline" style="padding-top:122px;">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p class="copyright">Company Name © 2016</p>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Dynamically-Queue-Videos.js"></script>
+    <script src="https://www.youtube.com/iframe_api"></script>
+</body>
+
+</html>
---
+++ model/meetTeam.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartVid</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
+    <link rel="stylesheet" href="assets/css/Dynamically-Queue-Videos.css">
+    <link rel="stylesheet" href="assets/css/Features-Blue.css">
+    <link rel="stylesheet" href="assets/css/Features-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean1.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button1.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+    <link rel="stylesheet" href="assets/css/Team-Grid.css">
+    <link rel="stylesheet" href="assets/css/Team-with-rotating-cards.css">
+</head>
+
+<body id="body">
+    <nav class="navbar navbar-default navigation-login" id="LoginNav">
+        <div class="container">
+            <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">SmartVid </a>
+                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+            </div>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="active" role="presentation"><a href="Main.html">Main </a></li>
+                    <li role="presentation"><a href="#">About </a></li>
+                </ul>
+                <p class="navbar-text navbar-right actions"><a class="navbar-link login" href="Main.html">Logout </a> <a class="btn btn-primary action-button" role="button" href="ProfDash.html">Home </a></p>
+            </div>
+        </div>
+    </nav>
+    <h1 id="head"><strong>Meet the Brogrammers</strong></h1>
+    <div class="team-boxed" style="background-color:rgba(238,244,247,0);">
+        <div class="container">
+            <div class="intro">
+                <p class="text-center" id="p1" style="color:rgb(225,229,231);">Meet the hard-working "Bros" that brought you SmartVid and the doc with the vision!</p>
+            </div>
+            <div class="row people" id="bros" style="margin:-4px;">
+                <div class="col-md-4 col-sm-6 item" style="height:965px;margin:3px;">
+                    <div class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:325px;height:458px;padding-top:18px;padding-bottom:72px;margin:-5px;"><img class="img-circle" src="assets/img/steven.jpeg">
+                        <h3 class="name" style="color:#f7f0f0;">Steven Biedenbach</h3>
+                        <p class="title">Lead UI designer</p>
+                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    </div>
+                    <div class="box" style="background-color:rgba(102,99,99,0.2);color:rgb(76,81,101);width:325px;padding:35px;padding-top:32px;margin:-2px;"><img class="img-circle" src="assets/img/alexpic.jpg">
+                        <h3 class="name" style="color:#f7f0f0;">Alex McWhorter</h3>
+                        <p class="title">team manager</p>
+                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 item" style="height:546px;margin:-13px;">
+                    <div class="row">
+                        <div class="col-md-12" style="height:492px;margin:-3px;padding:-15px;">
+                            <div class="box" style="background-color:rgba(102,99,99,0.2);color:rgb(76,81,101);width:325px;"><img class="img-circle" src="assets/img/Dr_Fen.jpg">
+                                <h3 class="name" style="color:#f7f0f0;">Dr. Fencil Shuler</h3>
+                                <p class="title">professor with a vison</p>
+                                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="sagar" class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:325px;"><img class="img-circle" src="assets/img/Sagar.jpg">
+                        <h3 class="name" style="color:#f7f0f0;">Sagar Hansalia</h3>
+                        <p class="title">Data modeler</p>
+                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 item" style="margin:-20px;">
+                    <div class="box" style="background-color:rgba(102,99,99,0.2);color:rgb(76,81,101);width:325px;"><img class="img-circle" src="assets/img/Cal.png">
+                        <h3 class="name" style="color:#f7f0f0;">Cal Gordon</h3>
+                        <p class="title">code architect</p>
+                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer id="foot">
+        <ul class="list-inline" style="padding-top:122px;">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p class="copyright">Company Name © 2016</p>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Dynamically-Queue-Videos.js"></script>
+    <script src="https://www.youtube.com/iframe_api"></script>
+</body>
+
+</html>
