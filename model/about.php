14219e67b2327b200e905b8e234a2b2ffa925a1d
Alex McWhorter <amcwhor1@ggc.edu>
2017-10-22 13:44:14 -0400

    made changes to about and meetTeam pages

--- model/about.php
+++ model/about.php
@@ -5,23 +5,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartVid</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../view/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
-    <link rel="stylesheet" href="assets/css/Dynamically-Queue-Videos.css">
-    <link rel="stylesheet" href="assets/css/Features-Blue.css">
-    <link rel="stylesheet" href="assets/css/Features-Boxed.css">
-    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/Navigation-Clean1.css">
+    <link rel="stylesheet" href="../view/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="../view/fonts/material-icons.min.css">
+    <link rel="stylesheet" href="../view/css/Dynamically-Queue-Videos.css">
+    <link rel="stylesheet" href="../view/css/Features-Blue.css">
+    <link rel="stylesheet" href="../view/css/Features-Boxed.css">
+    <link rel="stylesheet" href="../view/css/Footer-Basic.css">
+    <link rel="stylesheet" href="../view/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="../view/css/Navigation-Clean1.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
-    <link rel="stylesheet" href="assets/css/Navigation-with-Button1.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
-    <link rel="stylesheet" href="assets/css/Team-Grid.css">
-    <link rel="stylesheet" href="assets/css/Team-with-rotating-cards.css">
+    <link rel="stylesheet" href="../view/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="../view/css/Navigation-with-Button1.css">
+    <link rel="stylesheet" href="../view/css/styles.css">
+    <link rel="stylesheet" href="../view/css/Team-Boxed.css">
+    <link rel="stylesheet" href="../view/css/Team-Grid.css">
+    <link rel="stylesheet" href="../view/css/Team-with-rotating-cards.css">
 </head>
 
 <body id="body">
@@ -40,7 +40,7 @@
         </div>
     </nav>
     <div id="head">
-        <h1 class="text-primary" style="padding:0px;padding-bottom:-5px;margin:23px;height:0px;">What is SmartVid?</h1>
+        <h1 class="text-primary" id="title" style="padding:0px;padding-bottom:-5px;margin:23px;height:0px;">What is SmartVid?</h1>
         <p class="text-center" id="smartvidOverview" style="font-size:19px;/*background-color:rgba(25,25,31,0.54);*/width:1094px;max-width:-1px;min-width:1px;padding:4px;padding-right:1px;padding-top:0px;padding-left:119px;color:rgb(238,238,241);margin:83px;height:31px;">SmartVid is an educational platform for instructors to upload video content to help their students learn material at their own pace. Click the button below to meet the Brogrammers or check out some of our awesome features. Thank you for choosing
             SmartVid to help making learning great again!</p><a class="btn btn-default btn-lg" role="button" href="#" id="meetTheTeam" style="margin:-2px;padding:5px;padding-bottom:15px;padding-top:9px;height:41px;font-size:16px;">Meet the Team</a></div>
     <div>
@@ -50,11 +50,13 @@
                     <h2 class="text-primary" id="features" style="padding-bottom:20px;padding-left:2px;padding-top:27px;background-color:rgba(255,255,255,0);width:1138px;margin:10px;"><strong>Top Professor Features</strong> </h2></div>
             </div>
             <div class="row">
-                <div class="col-lg-4 col-md-4"><i class="glyphicon glyphicon-upload" style="font-size:45px;padding-bottom:-12px;"></i>
+                <div class="col-lg-4 col-md-4">
+                    <i class="glyphicon glyphicon-upload" style="font-size:45px;padding-bottom:-12px;"></i>
                     <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Upload Videos</h3>
                     <p id="uploadDescription">Professors can upload as many video lessons as they wish for their students to learn.</p>
                 </div>
-                <div class="col-lg-4 col-md-4" id="account"><i class="glyphicon glyphicon-lock" style="font-size:45px;padding-bottom:-12px;"></i>
+                <div class="col-lg-4 col-md-4" id="account">
+                    <i class="glyphicon glyphicon-lock" style="font-size:45px;padding-bottom:-12px;"></i>
                     <h3 style="padding:-15px;padding-top:-18px;margin:3px;">Protected Account</h3>
                     <p>Students and professors both have password protected accounts, But as a professor, no one else will be able to manage your class sections</p>
                 </div>
