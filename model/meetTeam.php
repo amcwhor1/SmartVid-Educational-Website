95d13d153afd112592d3b1a00ad625e70f836fa0
Alex McWhorter <amcwhor1@ggc.edu>
2017-10-22 13:44:14 -0400

    made changes to about and meetTeam pages

--- model/meetTeam.php
+++ model/meetTeam.php
@@ -5,26 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartVid</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../view/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
-    <link rel="stylesheet" href="assets/css/Dynamically-Queue-Videos.css">
-    <link rel="stylesheet" href="assets/css/Features-Blue.css">
-    <link rel="stylesheet" href="assets/css/Features-Boxed.css">
-    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/Navigation-Clean1.css">
+    <link rel="stylesheet" href="../view//fonts/ionicons.min.css">
+    <link rel="stylesheet" href="../view/fonts/material-icons.min.css">
+    <link rel="stylesheet" href="../view/css/Dynamically-Queue-Videos.css">
+    <link rel="stylesheet" href="../view/css/Features-Blue.css">
+    <link rel="stylesheet" href="../view/css/Features-Boxed.css">
+    <link rel="stylesheet" href="../view/css/Footer-Basic.css">
+    <link rel="stylesheet" href="../view/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="../view/css/Navigation-Clean1.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
-    <link rel="stylesheet" href="assets/css/Navigation-with-Button1.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
-    <link rel="stylesheet" href="assets/css/Team-Grid.css">
-    <link rel="stylesheet" href="assets/css/Team-with-rotating-cards.css">
+    <link rel="stylesheet" href="../view/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="../view/css/Navigation-with-Button1.css">
+    <link rel="stylesheet" href="../view/css/styles.css">
+    <link rel="stylesheet" href="../view/css/Team-Boxed.css">
+    <link rel="stylesheet" href="../view/css/Team-Grid.css">
+    <link rel="stylesheet" href="../view/css/Team-with-rotating-cards.css">
 </head>
 
-<body id="body">
+<body id="body" style="background-image:url(&quot;../view/img/mountainLights.jpg&quot;);background-size: cover;background-repeat:no-repeat;background-color:rgb(237,252,250);">
     <nav class="navbar navbar-default navigation-login" id="LoginNav">
         <div class="container">
             <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">SmartVid </a>
@@ -39,46 +39,46 @@
             </div>
         </div>
     </nav>
-    <h1 id="head"><strong>Meet the Brogrammers</strong></h1>
+    <h1 id="head"style="text-align:center;"><strong>Meet the Brogrammers</strong></h1>
+    <p class="text-center" id="p1" style="color:rgb(225,229,231);">Meet the hard-working "Bros" that brought you SmartVid and the doc with the vision!</p>
     <div class="team-boxed" style="background-color:rgba(238,244,247,0);">
-        <div class="container">
-            <div class="intro">
-                <p class="text-center" id="p1" style="color:rgb(225,229,231);">Meet the hard-working "Bros" that brought you SmartVid and the doc with the vision!</p>
-            </div>
-            <div class="row people" id="bros" style="margin:-4px;">
-                <div class="col-md-4 col-sm-6 item" style="height:965px;margin:3px;">
-                    <div class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:325px;height:458px;padding-top:18px;padding-bottom:72px;margin:-5px;"><img class="img-circle" src="assets/img/steven.jpeg">
+        <div class="container" style="width:1011px;">
+            <div class="row people" id="bros" style="margin:-9px;width:1002px;">
+                <div class="col-md-4 col-sm-6 item" style="height:480px;margin:0px;padding:2px;padding-right:38px;padding-left:-4px;padding-top:0px;width:318px;">
+                    <div id="steven" class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:300px;height:458px;padding:31px;padding-top:18px;padding-bottom:72px;margin:1px;"><img class="img-circle" src="../view/img/steven.jpg" style="height:160px;width:160px;">
                         <h3 class="name" style="color:#f7f0f0;">Steven Biedenbach</h3>
                         <p class="title">Lead UI designer</p>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                    </div>
-                    <div class="box" style="background-color:rgba(102,99,99,0.2);color:rgb(76,81,101);width:325px;padding:35px;padding-top:32px;margin:-2px;"><img class="img-circle" src="assets/img/alexpic.jpg">
-                        <h3 class="name" style="color:#f7f0f0;">Alex McWhorter</h3>
-                        <p class="title">team manager</p>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                        <p class="description" style="color:rgb(236,239,241);">Steven is a strong front-end developer that strived for simplicity in creating the professor dashboard.</p>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 item" style="height:546px;margin:-13px;">
-                    <div class="row">
-                        <div class="col-md-12" style="height:492px;margin:-3px;padding:-15px;">
-                            <div class="box" style="background-color:rgba(102,99,99,0.2);color:rgb(76,81,101);width:325px;"><img class="img-circle" src="assets/img/Dr_Fen.jpg">
-                                <h3 class="name" style="color:#f7f0f0;">Dr. Fencil Shuler</h3>
-                                <p class="title">professor with a vison</p>
-                                <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="sagar" class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:325px;"><img class="img-circle" src="assets/img/Sagar.jpg">
+                <div class="col-md-4 col-sm-6 item" style="height:480px;margin:0px;width:328px;">
+                    <div id="sagar" class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:300px;margin:1px;height:458px;"><img class="img-circle" src="../view/img/Sagar.jpg" id="sagarPic">
                         <h3 class="name" style="color:#f7f0f0;">Sagar Hansalia</h3>
                         <p class="title">Data modeler</p>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                        <p class="description" style="color:rgb(248,250,251);">The man in charge of creating and maintaining SmartVid's database and account infrastructure..</p>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 item" style="margin:-20px;">
-                    <div class="box" style="background-color:rgba(102,99,99,0.2);color:rgb(76,81,101);width:325px;"><img class="img-circle" src="assets/img/Cal.png">
+                <div class="col-md-4 col-sm-6 item" style="margin:0px;padding-left:19px;height:480px;padding-right:0px;width:328px;">
+                    <div id="cal" class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:300px;padding:29px;padding-left:25px;height:458px;"><img class="img-circle" src="../view/img/Cal.png" style="height:159px;width:160px;">
                         <h3 class="name" style="color:#f7f0f0;">Cal Gordon</h3>
                         <p class="title">code architect</p>
-                        <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa. </p>
+                        <p class="description" style="color:rgb(248,249,249);">Cal is the back-end developer that connects the database to website to make using SmartVId a seamless process. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row people" id="bros" style="margin:-4px;">
+                <div class="col-md-4 col-sm-6 item" style="height:480px;margin:0px;padding:2px;width:318px;">
+                    <div class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:300px;height:458px;padding-top:18px;padding-bottom:72px;margin:-5px;"><img class="img-circle" src="../view/img/alexpic.jpg" style="height:160px;width:160px;">
+                        <h3 class="name" style="color:#f7f0f0;">Alex McWhorter</h3>
+                        <p class="title">Team manager/ doc. lead</p>
+                        <p class="description" style="padding-right:0px;color:rgb(248,252,255);">As manager, Alex has had a hand in all areas of the project as well as communicating directly with client to make sure requirements are being met.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 item" style="height:480px;margin:0px;padding:3px;padding-left:13px;">
+                    <div id="sagar" class="box" style="background-color:rgba(3,3,3,0.6);color:rgb(76,81,101);width:300px;height:458px;margin:-5px;padding-left:30px;"><img class="img-circle" src="../view/img/Dr_Fen.jpg" style="height:160px;width:160px;">
+                        <h3 class="name" style="color:#f7f0f0;">Dr. Fencil Shuler</h3>
+                        <p class="title">client </p>
+                        <p class="description" style="color:rgb(248,252,254);">Chemistry professor at GGC that had the vision of SmartVid. Dr. Fen wanted a website to track his student's learning habits. </p>
                     </div>
                 </div>
             </div>
